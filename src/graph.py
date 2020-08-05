28dbe6ccd99079682f58e0d8a6c81ac94651eaa5
Colby Roberts <colbiverse@gmail.com>
2020-08-04 17:05:25 -0700

    made depth_first_search in the graph class

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,6 @@
 from node import Node
 
+
 class Graph:
     def __init__(self, edges, vertices):
         self.edges = edges
@@ -9,15 +10,17 @@ class Graph:
     def make_nodes(self):
         uncorrected_edges = [i for edge in self.edges for i in edge]
 
-        corrected_edges = self.remove_duplicate_edges(uncorrected_edges) #no more duplicates
+        corrected_edges = self.remove_duplicate_edges(
+            uncorrected_edges)  # no more duplicates
 
-        self.nodes = [Node(i) for i in corrected_edges] #make the nodes (but without values or neighbors)
+        # make the nodes (but without values or neighbors)
+        self.nodes = [Node(i) for i in corrected_edges]
 
         for i, node in enumerate(self.nodes):  # give the nodes their values
             node.set_value(self.vertices[i])
 
-        for x, y in self.edges: #give the nodes their neighbors
-            self.nodes[x].set_neighbor(self.nodes[y]) 
+        for x, y in self.edges:  # give the nodes their neighbors
+            self.nodes[x].set_neighbor(self.nodes[y])
 
     def remove_duplicate_edges(self, edges):
         corrected_edges = []
@@ -26,5 +29,19 @@ class Graph:
                 corrected_edges.append(edge)
 
         return corrected_edges
-                
 
+    def depth_first_search(self, index, visited_nodes=[]):
+        un_visited_neighbors = []
+
+        if self.nodes[index] not in visited_nodes:
+            visited_nodes.append(self.nodes[index])
+
+        for neighbor in self.nodes[index].neighbors:
+            if neighbor not in visited_nodes:
+                un_visited_neighbors.append(neighbor)
+
+        if un_visited_neighbors != []:
+            for node in un_visited_neighbors:
+                self.depth_first_search(node.index, visited_nodes)
+
+        return [node.index for node in visited_nodes]
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -8,13 +8,16 @@ edges = [(0, 1), (1, 2), (1, 3), (3, 4), (1, 4)]
 vertices = ['a', 'b', 'c', 'd', 'e']
 graph = Graph(edges, vertices)
 
-print('    Node Indices', [node.index for node in graph.nodes])
+print('    Node Indices:', [node.index for node in graph.nodes])
 assert [node.index for node in graph.nodes] == [0, 1, 2, 3, 4], "the node's indices in graph are wrong and should be [0, 1, 2, 3, 4] but was {}".format([node.index for node in graph.nodes])
 
-print('    Node Values', [node.value for node in graph.nodes])
+print('    Node Values:', [node.value for node in graph.nodes])
 assert [node.value for node in graph.nodes] == ['a', 'b', 'c', 'd', 'e'], "the node's values in graph are wrong and should be [0, 1, 2, 3, 4] but was {}".format([node.value for node in graph.nodes])
 
-print('    Number of Neighbors for each node', [len(node.neighbors) for node in graph.nodes])
+print('    Number of Neighbors for each node:', [len(node.neighbors) for node in graph.nodes])
 assert [len(node.neighbors) for node in graph.nodes] == [1, 4, 1, 2, 2], "the node's values in graph are wrong and should be [0, 1, 2, 3, 4] but was {}".format([node.value for node in graph.nodes])
+
+print('    Depth First Search:', graph.depth_first_search(0))
+assert graph.depth_first_search(0) == [0, 1, 2, 3, 4], "the search's values in graph are wrong and should be [0, 1, 2, 3, 4] but was {}".format(graph.depth_first_search(0))
 
 print('All Tests PASS!!!')
